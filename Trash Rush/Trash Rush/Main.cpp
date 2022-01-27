11104f937e7880548ad98d542631cd2ba58f0d69
mumop <mumopeter17@gmail.com>
2022-01-27 23:50:07 +0300

    Vertex and fragment shaders

--- Trash Rush/Trash Rush/Main.cpp
+++ Trash Rush/Trash Rush/Main.cpp
@@ -1,36 +1,149 @@
-#include "Trashcan.h"
-
-#include<GL/glut.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include <string>
-#include<math.h>
+#include <cstdlib>
 
+#include "Trashcan.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-void init() {
-	glClearColor(1.0, 1.0, 1.0, 1.0);
-}
+#include <iostream>
 
-Trashcan green("Organic", "green");
-Trashcan blue("Plastic", "blue");
-Trashcan red("Hazardous", "red");
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT);
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"layout (location = 2) in vec2 aTexCoord;\n"
+"out vec3 ourColor;\n"
+"out vec2 TexCoord;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   ourColor = aColor;\n"
+"   TexCoord = aTexCoord;\n"
+"}\0";
 
-	green.displayTrashcan();
-	blue.displayTrashcan();
-    red.displayTrashcan();
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"in vec2 TexCoord;\n"
+"uniform sampler2D ourTexture;\n"
+"void main()\n"
+"{\n"
+"   FragColor = texture(ourTexture, TexCoord);\n"
+"}\n\0";
 
-	glFlush();
-}
+const char* fragmentShaderTrashcan = "#version 330 core\n"
+"out vec3 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec3(ourColor);\n"
+"}\n\0";
 
-int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitWindowSize(1000, 1000);
-	glutInitWindowPosition(50, 100);
-	glutCreateWindow("Trash Rush");
-	init();
-	glutDisplayFunc(display);
-	glutMainLoop();
-}
+int main()
+{
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Trash Rush Game", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // glad: load all OpenGL function pointers
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    // fragment shader for trashcan
+
+    unsigned int fragmentShaderCan = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShaderCan, 1, &fragmentShaderTrashcan, NULL);
+    glCompileShader(fragmentShaderCan);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShaderCan, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShaderCan, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // link shaders
+    unsigned int shaderProgram = glCreateProgram();
+    unsigned int shaderProgramTrashcan = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glAttachShader(shaderProgramTrashcan, vertexShader);
+    glAttachShader(shaderProgramTrashcan, fragmentShaderCan);
+    glLinkProgram(shaderProgramTrashcan);
+    // check for linking errors
+    glGetProgramiv(shaderProgramTrashcan, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgramTrashcan, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glDeleteShader(fragmentShaderCan);
